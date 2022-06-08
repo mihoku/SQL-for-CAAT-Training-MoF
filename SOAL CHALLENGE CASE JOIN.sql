b13ef32cc58da0555c1e8e7c6b29f2daed1f91c2
mihoku <49892705+mihoku@users.noreply.github.com>
2022-06-08 17:45:38 +0700

    Update SOAL CHALLENGE CASE JOIN.sql

--- SOAL CHALLENGE CASE JOIN.sql
+++ SOAL CHALLENGE CASE JOIN.sql
@@ -2,7 +2,9 @@
 
 --Berapa jumlah pegawai dari masing-masing manager (sebutkan juga nama manager-nya), dan sebutkan apa jabatan (job title) dari manager tersebut. 
 
-select E.FIRST_NAME, E.LAST_NAME, J.JOB_TITLE, COUNT(*) JUMLAH_PEGAWAI from EMPLOYEES E
+select E.EMPLOYEE_ID, E.FIRST_NAME, E.LAST_NAME, J.JOB_TITLE, COUNT(*) JUMLAH_PEGAWAI 
+
+from EMPLOYEES E
 
 join EMPLOYEES E2
 ON E2.MANAGER_ID = E.EMPLOYEE_ID
@@ -10,4 +12,4 @@ ON E2.MANAGER_ID = E.EMPLOYEE_ID
 JOIN JOBS J
 ON J.JOB_ID = E.JOB_ID
 
-GROUP BY E.FIRST_NAME, E.LAST_NAME, J.JOB_TITLE
+GROUP BY E.EMPLOYEE_ID, E.FIRST_NAME, E.LAST_NAME, J.JOB_TITLE

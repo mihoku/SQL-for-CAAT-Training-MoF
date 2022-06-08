611a69b09c448a9089bbdb5823b387aa9462199f
mihoku <49892705+mihoku@users.noreply.github.com>
2022-06-08 21:28:36 +0700

    Update SOAL FUNCTIONS 3.3.sql

--- SOAL FUNCTIONS 3.3.sql
+++ SOAL FUNCTIONS 3.3.sql
@@ -1,4 +1,6 @@
 --SOAL 3
+/*Sebutkan 3 JOB_ID yang memiliki maksimum gaji terbesar di perusahaan berdasarkan gaji
+actual (salary) pada table EMPLOYEES.*/
 
 SELECT TOP 3 JOB_ID, MAX(SALARY) MAX_SALARY FROM EMPLOYEES
 GROUP BY JOB_ID

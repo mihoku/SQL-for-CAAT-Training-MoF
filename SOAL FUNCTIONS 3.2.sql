fa66207612bb7bfa81a827fd8838b329fd8e329d
mihoku <49892705+mihoku@users.noreply.github.com>
2022-06-08 21:27:54 +0700

    Update SOAL FUNCTIONS 3.2.sql

--- SOAL FUNCTIONS 3.2.sql
+++ SOAL FUNCTIONS 3.2.sql
@@ -1,4 +1,6 @@
 --SOAL 2 SEHARUSNYA MATERINYA BELUM MASUK KARENA SUBQUERY
+/*Berapa orang pegawai dengan jabatan (job_id) berakhiran _clerk 
+yang gajinya di bawah ratarata dibanding gaji seluruh pegawai di perusahaan?*/
 
 SELECT * FROM EMPLOYEES
 WHERE SALARY<(SELECT AVG(SALARY) FROM EMPLOYEES)

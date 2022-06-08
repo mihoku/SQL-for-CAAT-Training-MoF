68346a844f2fdcdcfeee53799fe8a2288c2c9cd2
mihoku <49892705+mihoku@users.noreply.github.com>
2022-06-08 21:29:40 +0700

    Update SOAL FUNCTIONS 3.4.sql

--- SOAL FUNCTIONS 3.4.sql
+++ SOAL FUNCTIONS 3.4.sql
@@ -1,4 +1,6 @@
 --SOAL 4
+/*Sebutkan 3 manager_id dengan pegawai terbanyak, dan berapa masing-masing rata-rata gaji
+pegawai di tim manajer tersebut.*/
 
 SELECT TOP 3 MANAGER_ID, COUNT(*) JUMLAH_PEGAWAI, AVG(SALARY) MAX_SALARY FROM EMPLOYEES
 GROUP BY MANAGER_ID
